40e1e1fd84c698b008967a5d14024ff3973859c6
Diogo Correia <dv_correia@hotmail.com>
2026-09-09 00:14:59 +0100

    refactor(secrets): create admin group to assign to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,28 +1,19 @@
 let
   sshKeys = import ../ssh-keys.nix;
-  inherit (sshKeys) dvcorreia afonso lena;
+
+  inherit (sshKeys) lena;
+
+  admins = [
+    sshKeys.dvcorreia
+    sshKeys.afonso
+  ];
 in
 {
-  "hetzner-api-token.age".publicKeys = [
-    dvcorreia
-    afonso
-  ];
-  "opentofu-encryption-key.age".publicKeys = [
-    dvcorreia
-    afonso
-  ];
-  "cloudflare-dns-token.age".publicKeys = [
-    dvcorreia
-    afonso
-  ];
-  "pocket-id.age".publicKeys = [
-    dvcorreia
-    afonso
-    lena
-  ];
-  "headscale-oidc.age".publicKeys = [
-    dvcorreia
-    afonso
-    lena
-  ];
+  "hetzner-api-token.age".publicKeys = admins;
+  "opentofu-encryption-key.age".publicKeys = admins;
+  "cloudflare-dns-token.age".publicKeys = admins;
+
+  "pocket-id.age".publicKeys = [ lena ] ++ admins;
+  "headscale-oidc.age".publicKeys = [ lena ] ++ admins;
+  "lena-tailscale-preauth-key.age".publicKeys = [ lena ] ++ admins;
 }
